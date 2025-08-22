39eadf0f019036e9c66735a1a76da0320ad43f2b
JiksGit <2wlrlwkd@gmail.com>
2025-08-22 16:34:51 +0900

    Feat : Stomp Chat

--- backend/boot_workspace/stompchat/src/main/java/com/sinse/stompchat/controller/ChatController.java
+++ backend/boot_workspace/stompchat/src/main/java/com/sinse/stompchat/controller/ChatController.java
@@ -7,13 +7,13 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListSet;
 
 
 @Slf4j
@@ -23,6 +23,14 @@ public class ChatController {
     private Set<String> connectedUsers = new ConcurrentHashMap<>().newKeySet();
 
     private Map<String, ChatRoom> roomStorage = new ConcurrentHashMap<>();
+
+    /* favicon 처리
+    1) static 디렉토리에 실제로 favicon 이미지를 보유
+    2) 컨트롤러에 요청을 처리하되, 이미지를 반환하지 않고 void
+     */
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    public void favicon() {}
 
     /*클라이언트의 접속 요청 처리
      *   클라이언트가 7777/app/connect로 접속하여 이 메서드 실행
@@ -42,11 +50,24 @@ public class ChatController {
         return connectedUsers;
     }
 
+    @MessageMapping("/disconnect")
+    @SendTo("/topic/users")
+    public Set<String> disconnect(ChatMessage chatMessage) throws Exception {
+        connectedUsers.remove(chatMessage.getSender());
+        return connectedUsers;
+    }
+
     // 클라이언트의 메시지 전송 처리
     @MessageMapping("/chat.send")
     @SendTo("/topic/messages")
     public ChatMessage send(ChatMessage chatMessage) throws Exception {
         return chatMessage;
+    }
+
+    @MessageMapping("/room.list")
+    @SendTo("/topic/rooms")
+    public Collection<ChatRoom> getRoomList() throws Exception {
+        return roomStorage.values();
     }
 
     // 방만들기 메시지 전송 처리
